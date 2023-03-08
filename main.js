22647282e29f8f6a7910aa574d9c00cc11083a52
Taylan Mun <taylanmun@gmail.com>
2023-03-08 21:50:13 +0300

    adding jumping, happy, sad numbers,remove limitation properties, adding minus property

--- main.js
+++ main.js
@@ -11,8 +11,8 @@ document.addEventListener('DOMContentLoaded', function () {
     const splitRequestArr = request.split(" ");
     const startingFrom = splitRequestArr[0]
     const lengthOfList = splitRequestArr[1] || false
-    const propertyOne = splitRequestArr[2] || false
-    const propertyTwo = splitRequestArr[3] || false
+
+    const properties = splitRequestArr.slice(2) || false
 
     if(request === ""){
       document.querySelector(".invalid-feedback").innerHTML = "Please enter a request."
@@ -43,29 +43,67 @@ document.addEventListener('DOMContentLoaded', function () {
       errorCount++;
     }
 
-    if(propertyOne && !(propertyOne in availableProperties)){
-      thirdParameterError = `${errorCount > 0 ? "</br></br>":""}The property [${propertyOne.toUpperCase()}] is wrong.</br>Available properties: [${Object.keys(availableProperties).map(key => key.toUpperCase()).join(", ")}]`;
-      document.querySelector(".invalid-feedback").innerHTML =  firstParameterError + secondParameterError + thirdParameterError
-      numberElement.classList.add("is-invalid");
-      document.querySelector('.result').innerHTML = "";
-      errorCount++;
+    let wrongArray = [];
+    if(properties){
+      properties.forEach(p => {
+        let checkNegative = p.includes("-");
+        let availableProperty = checkNegative ? p.slice(1): p
+        if(!(availableProperty in availableProperties)){
+          wrongArray.push(p)
+        }
+      })
+
+      if(wrongArray.length > 0){
+        thirdParameterError = `${errorCount > 0 ? "</br></br>":""}${wrongArray.length > 1 ? `The properties [${wrongArray.join(", ").toUpperCase()}] are` : `The property ${wrongArray[0].toUpperCase()}` } wrong.</br>Available properties: [${Object.keys(availableProperties).map(key => key.toUpperCase()).join(", ")}]`;
+        document.querySelector(".invalid-feedback").innerHTML =  firstParameterError + secondParameterError + thirdParameterError
+        numberElement.classList.add("is-invalid");
+        document.querySelector('.result').innerHTML = "";
+        errorCount++;
+      }
     }
 
-
-    if(propertyTwo && !(propertyTwo in availableProperties)){
-      fourthParameterError = `${firstParameterError !== "" || secondParameterError !== "" ? "</br></br>":""}${thirdParameterError !== "" ? `The properties [${propertyOne.toUpperCase()}, ${propertyTwo.toUpperCase()}] are` : `The property [${propertyOne.toUpperCase()}] is`} wrong.</br>Available properties: [${Object.keys(availableProperties).map(key => key.toUpperCase()).join(", ")}]`;
-
-      document.querySelector(".invalid-feedback").innerHTML =  firstParameterError + secondParameterError + fourthParameterError
-      numberElement.classList.add("is-invalid");
-      document.querySelector('.result').innerHTML = "";
-      errorCount++;
+    const errorProperties = {
+      even: "-even,odd",
+      '-even': "-odd,even",
+      odd: "-odd,even",
+      '-odd': "-even,odd",
+      square: "-square,sunny",
+      '-square': "-sunny,square",
+      sunny: "-sunny,square",
+      '-sunny': "-square,sunny",
+      duck: "-duck,spy",
+      '-duck': "-spy,duck",
+      spy: "-spy,duck",
+      '-spy': "-duck,spy",
+      happy: "-happy, sad", 
+      '-happy': "-sad, happy", 
+      sad: "-sad,happy",
+      '-sad': "-happy,sad",
     }
 
-    if(propertyTwo && (splitRequestArr.includes('even') && splitRequestArr.includes('odd')) || (splitRequestArr.includes('square') && splitRequestArr.includes('sunny')) || (splitRequestArr.includes('duck') && splitRequestArr.includes('spy'))){
-      document.querySelector(".invalid-feedback").innerHTML =  `${document.querySelector(".invalid-feedback").innerHTML}${errorCount > 0 ? "</br></br>" : ""}The request contains mutually exclusive properties: [${propertyOne}, ${propertyTwo}]</br>There are no numbers with these properties.`;
-      numberElement.classList.add("is-invalid");
-      document.querySelector('.result').innerHTML = "";
-      errorCount++;
+    let foundErrorString = "";
+    let index = 0;
+    if(properties){
+      while(foundErrorString === "" && index < properties.length){
+        if(properties[index] in errorProperties){
+          let propertyImpossible = errorProperties[properties[index]].split(',')
+      
+          if(properties.includes(propertyImpossible[0])){
+            foundErrorString = `${properties[index]}, ${propertyImpossible[0]}`
+          }
+          if(properties.includes(propertyImpossible[1])){
+            foundErrorString = `${properties[index]}, ${propertyImpossible[1]}`
+          }
+        }
+        index++;
+      }
+
+      if(foundErrorString !== ""){
+        document.querySelector(".invalid-feedback").innerHTML =  `${document.querySelector(".invalid-feedback").innerHTML}${errorCount > 0 ? "</br></br>" : ""}The request contains mutually exclusive properties: [${foundErrorString.toUpperCase()}]</br>There are no numbers with these properties.`;
+        numberElement.classList.add("is-invalid");
+        document.querySelector('.result').innerHTML = "";
+        errorCount++;
+      }
     }
 
     if(lengthOfList === false){
@@ -80,24 +118,65 @@ document.addEventListener('DOMContentLoaded', function () {
     }
 
     if(lengthOfList && errorCount === 0){
-      document.querySelector('.result').innerHTML = getPropertiesNumbers(startingFrom, lengthOfList, propertyOne, propertyTwo);
+      document.querySelector('.result').innerHTML = getPropertiesNumbers(startingFrom, lengthOfList, properties);
     }
   });
 })
 
-const isEven = (number) => Number(number) % 2 === 0 ? "even" : ""
-const isOdd = (number) => Number(number) % 2 !== 0 ? "odd" : ""
+const isEven = (number) => Number(number) % 2 === 0 ? "even" : "";
+
+const isOdd = (number) => Number(number) % 2 !== 0 ? "odd" : "";
+
 const isBuzz = (number) => Number(number) % 10 === 7 || number % 7 === 0 ? ", buzz" : "";
+
 const isDuck = (number) => number.toString().slice(1).includes("0") ? ", duck" : "";
+
 const isPalindromic= (number) => number.split("").reverse().join("") === number || Number(number) < 10 ? ", palindromic" : ""
+
 const isGapful = (number) => !(Number(number) < 100) && (Number(number) % Number(number.toString().slice(0,1) + number.toString().slice(-1))) === 0 ? ", gapful" : ""
+
 const isSpy = (number) => {
-  sumOfAllDigits = number.toString().split('').map(Number).reduce(function (a, b) { return a + b; }, 0);
-  productOfAllDigits = number.toString().split('').map(Number).reduce(function (a, b) { return a * b; }, 1);
+  sumOfAllDigits = number.toString().split('').map(Number).reduce(function (total, currentValue) { return total + currentValue; }, 0);
+  productOfAllDigits = number.toString().split('').map(Number).reduce(function (total, currentValue) { return total * currentValue; }, 1);
   return sumOfAllDigits === productOfAllDigits ? ", spy" : "";
 }
+
 const isSquare = (number) => Number.isInteger(Math.sqrt(Number(number))) ? ", square" : ""
+
 const isSunny = (number) => Number.isInteger(Math.sqrt(Number(number) + 1)) ? ", sunny" : ""
+
+const isJumping = (number) => {
+  let numberArr = number.toString().split('');
+  if(numberArr.length === 1) return ", jumping";
+  for(let i=1;i<numberArr.length;i++){
+     if(Math.abs(numberArr[i]-numberArr[i-1]) !== 1){
+        return "";
+     }
+  }
+  return ", jumping";
+}
+
+const isHappy = (number) => {
+  let isHappy = number;
+  let totalArray = [];
+  while(isHappy !== 1 && isHappy !== 0){
+    if(totalArray.includes(Number(isHappy))) return "";
+    totalArray.push(Number(isHappy));
+    isHappy = isHappy.toString().split("").map(Number).reduce(function (total, currentValue) { return total + Math.pow(currentValue,2); }, 0);
+  }
+  return isHappy === 1 ? ", happy" : "";
+}
+
+const isSad = (number) => {
+  let isSad = number;
+  let totalArray = [];
+  while(isSad !== 1 && isSad !== 0){
+    if(totalArray.includes(Number(isSad))) return ", sad";
+    totalArray.push(Number(isSad));
+    isSad = isSad.toString().split("").map(Number).reduce(function (total, currentValue) { return total + Math.pow(currentValue,2); }, 0);
+  }
+  return isSad === 0 ? ", sad" : "";
+}
 
 const availableProperties = {
   even: isEven,
@@ -109,6 +188,9 @@ const availableProperties = {
   spy:isSpy,
   sunny: isSunny,
   square: isSquare,
+  jumping: isJumping,
+  happy: isHappy,
+  sad: isSad,
 }
 
 
@@ -122,31 +204,41 @@ const getPropertiesOneNumber = (number) => {
             <div style="margin-left:62px">spy: ${Boolean(isSpy(number)).toString()}</div>
             <div style="margin-left:39px">square: ${Boolean(isSquare(number)).toString()}</div>
             <div style="margin-left:46px">sunny: ${Boolean(isSunny(number)).toString()}</div>
+            <div style="margin-left:46px">jumping: ${Boolean(isJumping(number)).toString()}</div>
+            <div style="margin-left:46px">happy: ${Boolean(isHappy(number)).toString()}</div>
+            <div style="margin-left:46px">sad: ${Boolean(isSad(number)).toString()}</div>
             <div style="margin-left:55px">even: ${Boolean(isEven(number)).toString()}</div>
             <div style="margin-left:61px">odd:  ${Boolean(isOdd(number)).toString()}</div>
           </div>
           `
 }
 
-const getPropertiesNumbers = (startingFrom, lengthOfList, propertyOne = null, propertyTwo = null) => {
+const getPropertiesNumbers = (startingFrom, lengthOfList, properties = []) => {
   let i = 0;
   let generatedCount = 0;
   let html = "";
   let number = startingFrom;
   while(generatedCount < lengthOfList){
-    if(propertyOne && propertyTwo){
-      if(Boolean(availableProperties[propertyOne](number)) && Boolean(availableProperties[propertyTwo](number))){
-        html += `${number} is ${isEven(number)}${isOdd(number)}${isBuzz(number)}${isDuck(number)}${isPalindromic(number)}${isGapful(number)}${isSpy(number)}${isSquare(number)}${isSunny(number)}</br>`
+    if(properties){
+      let checkGenerated = true;
+      properties.forEach(p => {
+        let property = p;
+        if(property.includes("-")){
+          property = property.slice(1);
+          if(Boolean(availableProperties[property](number))){
+            checkGenerated = false;
+          }
+        }else{
+          if(!Boolean(availableProperties[property](number))){
+            checkGenerated = false;
+          }
+        }
+      })
+
+      if(checkGenerated){
+        html += `${number} is ${isEven(number)}${isOdd(number)}${isBuzz(number)}${isDuck(number)}${isPalindromic(number)}${isGapful(number)}${isSpy(number)}${isSquare(number)}${isSunny(number)}${isJumping(number)}${isHappy(number)}${isSad(number)}</br>`
         generatedCount++
       }
-    }else if(propertyOne){
-      if(Boolean(availableProperties[propertyOne](number))){
-        html += `${number} is ${isEven(number)}${isOdd(number)}${isBuzz(number)}${isDuck(number)}${isPalindromic(number)}${isGapful(number)}${isSpy(number)}${isSquare(number)}${isSunny(number)}</br>`
-        generatedCount++
-      }
-    }else{
-      html += `${number} is ${isEven(number)}${isOdd(number)}${isBuzz(number)}${isDuck(number)}${isPalindromic(number)}${isGapful(number)}${isSpy(number)}${isSquare(number)}${isSunny(number)}</br>`
-      generatedCount++
     }
     i++;
     number =(Number(startingFrom) + i).toString();
